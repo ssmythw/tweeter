2d6b841d86d7dc4bea804548e872ad15d68e6339
Scott <ssmythw@outlook.com>
2022-12-19 12:02:06 -0500

    added screenshot

--- README.md
+++ README.md
@@ -9,10 +9,14 @@ This repository is the starter code for the project: Students will fork and clon
 1. [Create](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) a new repository using this repository as a template.
 2. Clone your repository onto your local device.
 3. Install dependencies using the `npm install` command.
-3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
-4. Go to <http://localhost:8080/> in your browser.
+4. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
+5. Go to <http://localhost:8080/> in your browser.
 
 ## Dependencies
 
 - Express
+- Nodemon
+- Body Parser
+- Chance
+- md5
 - Node 5.10.x or above
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,7 @@
 
 const tweetsContainer = document.getElementById("tweets-container");
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+const data = [];
 
 $("#my-form").submit((event) => {
   event.preventDefault();
@@ -61,7 +38,7 @@ $("#my-form").submit((event) => {
         content: {
           text: tweetTextVal,
         },
-        created_at: 1461116232227,
+        created_at: Date.now(),
       };
       $("#tweet-text").val("");
       data.unshift(user);
